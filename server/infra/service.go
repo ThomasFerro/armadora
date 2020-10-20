3fe07d60a208b05d28982c633dd717ac3c0a4264
Thomas FERRO <t.ferro184@gmail.com>
2020-10-20 08:38:16 +0200

    feat: check if a parrty exists before sending a command or getting his state

--- server/infra/service.go
+++ server/infra/service.go
@@ -20,20 +20,80 @@ type ArmadoraService struct {
 	partiesManager party.PartiesManager
 }
 
-// NewArmadoraService Create a new Armadora service
-func NewArmadoraService() ArmadoraService {
-	partiesRepository := party.NewPartiesMongoRepository(
-		config.GetConfiguration("MONGO_PARTY_COLLECTION_NAME"),
-	)
-
-	return ArmadoraService{
-		eventStore:     storage.NewEventStore(),
-		partiesManager: party.NewPartiesManager(partiesRepository),
+// GetVisibleParties Retrieve every available parties
+func (armadoraService ArmadoraService) GetVisibleParties() ([]party.PartyName, error) {
+	parties, err := armadoraService.partiesManager.GetVisibleParties()
+	if err != nil {
+		return nil, err
 	}
+	returnedParties := []party.PartyName{}
+	for _, partyFromEventStore := range parties {
+		returnedParties = append(returnedParties, partyFromEventStore.Name)
+	}
+	return returnedParties, nil
+}
+
+// CreateParty Create a new party
+func (armadoraService ArmadoraService) CreateParty() (party.PartyName, error) {
+	// TODO: Shorter name
+	// TODO: Private parties
+	partyName := party.PartyName(uuid.New().String())
+	newPartyName, err := armadoraService.partiesManager.CreateParty(partyName, true)
+	if err != nil {
+		return "", err
+	}
+	history, err := ManageCommand([]event.Event{}, Command{
+		CommandType: "CreateGame",
+	})
+	if err != nil {
+		return "", err
+	}
+	err = armadoraService.eventStore.AppendToHistory(
+		string(newPartyName),
+		"",
+		dto.ToEventsDto(history),
+	)
+	if err != nil {
+		return "", err
+	}
+	return newPartyName, nil
+}
+
+// GetPartyGameState Get the current state of a party's game
+func (armadoraService ArmadoraService) GetPartyGameState(partyName party.PartyName) (dto.GameDto, error) {
+	requestedPartyExists, err := partyExists(armadoraService.partiesManager, partyName)
+
+	if err != nil {
+		return dto.GameDto{}, fmt.Errorf("An error has occurred while checking if the party %v exists: %w", partyName, err)
+	}
+
+	if !requestedPartyExists {
+		return dto.GameDto{}, fmt.Errorf("The party %v does not exists", partyName)
+	}
+
+	history, err := armadoraService.eventStore.GetHistory(string(partyName))
+	if err != nil {
+		return dto.GameDto{}, err
+	}
+	return dto.ToGameDto(
+		game.ReplayHistory(
+			dto.FromEventsDto(history.Events),
+		),
+	), nil
 }
 
 // ReceiveCommand Manage a received command
 func (armadoraService ArmadoraService) ReceiveCommand(partyName party.PartyName, command Command) error {
+	requestedPartyExists, err := partyExists(armadoraService.partiesManager, partyName)
+
+	if err != nil {
+		return fmt.Errorf("An error has occurred while checking if the party %v exists: %w", partyName, err)
+	}
+
+	if !requestedPartyExists {
+		return fmt.Errorf("The party %v does not exists", partyName)
+	}
+
 	log.Printf("Receiving the following command for party %v: %v\n", partyName, command)
 
 	history, err := armadoraService.eventStore.GetHistory(string(partyName))
@@ -72,54 +132,25 @@ func (armadoraService ArmadoraService) ReceiveCommand(partyName party.PartyName,
 	return nil
 }
 
-// CreateParty Create a new party
-func (armadoraService ArmadoraService) CreateParty() (party.PartyName, error) {
-	// TODO: Shorter name
-	// TODO: Private parties
-	partyName := party.PartyName(uuid.New().String())
-	newPartyName, err := armadoraService.partiesManager.CreateParty(partyName, true)
-	if err != nil {
-		return "", err
+func partyExists(partiesManager party.PartiesManager, partyName party.PartyName) (bool, error) {
+	_, err := partiesManager.GetParty(partyName)
+	if _, partyNotFound := err.(party.NotFound); partyNotFound {
+		return false, nil
 	}
-	history, err := ManageCommand([]event.Event{}, Command{
-		CommandType: "CreateGame",
-	})
 	if err != nil {
-		return "", err
+		return false, err
 	}
-	err = armadoraService.eventStore.AppendToHistory(
-		string(newPartyName),
-		"",
-		dto.ToEventsDto(history),
+	return true, err
+}
+
+// NewArmadoraService Create a new Armadora service
+func NewArmadoraService() ArmadoraService {
+	partiesRepository := party.NewPartiesMongoRepository(
+		config.GetConfiguration("MONGO_PARTY_COLLECTION_NAME"),
 	)
-	if err != nil {
-		return "", err
-	}
-	return newPartyName, nil
-}
 
-// GetPartyGameState Get the current state of a party's game
-func (armadoraService ArmadoraService) GetPartyGameState(partyName party.PartyName) (dto.GameDto, error) {
-	history, err := armadoraService.eventStore.GetHistory(string(partyName))
-	if err != nil {
-		return dto.GameDto{}, err
+	return ArmadoraService{
+		eventStore:     storage.NewEventStore(),
+		partiesManager: party.NewPartiesManager(partiesRepository),
 	}
-	return dto.ToGameDto(
-		game.ReplayHistory(
-			dto.FromEventsDto(history.Events),
-		),
-	), nil
-}
-
-// GetVisibleParties Retrieve every available parties
-func (armadoraService ArmadoraService) GetVisibleParties() ([]party.PartyName, error) {
-	parties, err := armadoraService.partiesManager.GetVisibleParties()
-	if err != nil {
-		return nil, err
-	}
-	returnedParties := []party.PartyName{}
-	for _, partyFromEventStore := range parties {
-		returnedParties = append(returnedParties, partyFromEventStore.Name)
-	}
-	return returnedParties, nil
 }
